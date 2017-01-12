5fc46b8a7135bfe74ebdf946f9efab0fc468b3db
IvanM <ivan.malyshev.376@gmail.com>
2017-01-12 14:05:45 +0300

    es6

--- openfreelancer-server.js
+++ openfreelancer-server.js
@@ -1,23 +1,23 @@
-var db = require('mongodb').MongoClient;
-var ObjectID = new require('mongodb').ObjectID;
-var http = require('http');
-var fs = require('fs');
-var clients = require('./clients');
+const db = require('mongodb').MongoClient;
+const ObjectID = new require('mongodb').ObjectID;
+const http = require('http');
+const fs = require('fs');
+const clients = require('./clients');
 
-db.connect('mongodb://localhost:27017/openfreelancer', function (err, db) {
-    var screenshots = db.collection('screenshots');
+db.connect('mongodb://localhost:27017/openfreelancer', (err, db) => {
+    const screenshots = db.collection('screenshots');
     screenshots.createIndex({ client: 1, created_at: 1 });
     if (process.argv[2] === 'STATS') {
-        db.stats(function (err, stats){ console.log(stats); });
+        db.stats((err, stats) => console.log(stats || err));
     }
     if (process.argv[2] === 'CLEAR') {
-        screenshots.remove({ created_at: { $lt: Date.now() + 31 * 24 * 60 * 60 * 1000 }}, function () { console.log('done'); }); // older than month
+        screenshots.remove({ created_at: { $lt: Date.now() + 31 * 24 * 60 * 60 * 1000 }}, () => console.log('done')); // older than month
     }
-    http.createServer(function (req, res) {
-        var url    = req.url.split('/');
-        var id     = url[1];
-        var path   = url[2];
-        var client = clients[id];
+    http.createServer((req, res) => {
+        const url    = req.url.split('/');
+        const id     = url[1];
+        const path   = url[2];
+        const client = clients[id];
         if (id === 'openfreelancer-theme.css') {
             fs.createReadStream('openfreelancer-theme.css').pipe(res);
         } else if (!client) {
@@ -28,12 +28,11 @@ db.connect('mongodb://localhost:27017/openfreelancer', function (err, db) {
                     client.lock = 'first_request';
                 } else {
                     client.lock = 'timeout'; // DOS protection
-                    setTimeout(function () { delete client.lock; }, 10 * 60 * 1000 - 10000); // 10 minutes - 10 seconds
+                    setTimeout(() => { delete client.lock }, 10 * 60 * 1000 - 10000); // 10 minutes - 10 seconds
                 }
-                var body = [];
-                req.on('data', function (chunk) {
-                    body.push(chunk);
-                }).on('end', function () {
+                let body = [];
+                req.on('data', chunk => body.push(chunk))
+                   .on('end', () => {
                     res.end();
                     body = new Buffer.concat(body);
                     if (!client.cachedImage) {
@@ -52,19 +51,19 @@ db.connect('mongodb://localhost:27017/openfreelancer', function (err, db) {
             } else if (req.method === 'GET') {
                 if (path) {
                     try {
-                        screenshots.findOne(new ObjectID(path), function (err, item) {
-                            res.end(new Buffer(item.screenshot.buffer));
-                        });
-                    } catch (err) { res.end('Unknown id: ' + path); }
+                        screenshots.findOne(new ObjectID(path), (err, item) => res.end(new Buffer(item.screenshot.buffer)));
+                    } catch (err) {
+                        res.end('Unknown id: ' + path);
+                    }
                 } else {
                     screenshots.find({
                         created_at: {
                             $gt: Date.now() - 31 * 24 * 60 * 60 * 1000 // last month
                         },
                         client: id
-                    }).sort({ created_at: -1 }).toArray(function (err, items) {
+                    }).sort({ created_at: -1 }).toArray((err, items) => {
                         var body = '<head><link rel="stylesheet" type="text/css" href="openfreelancer-theme.css"><head>' + client.name + '<table>';
-                        items.forEach(function (item, index) {
+                        items.forEach((item, index) => {
                             if (!(index % 6)) {
                                 body += '</tr><tr><td>' + Math.floor(index/6) + '</td><td>' + new Date(item.created_at).toUTCString().slice(0, -7).split(' 2016 ').join(' ') + '</td>';
                             }
@@ -80,6 +79,4 @@ db.connect('mongodb://localhost:27017/openfreelancer', function (err, db) {
     }).listen(process.env.PORT || 3070);
 });
 
-process.on('uncaughtException', function (err) {
-    console.log(err.stack);
-});
+process.on('uncaughtException', err => console.log(err.stack));
